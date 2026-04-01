6a03c3098c3ab48f7aae751d1b30eb69496e43a9
Cẩm Tiên <phamthicamtienqn2020@gmail.com>
2026-04-01 12:55:28 +0700

    Tiên upload model

--- apps/schedules/admin.py
+++ apps/schedules/admin.py
@@ -1,17 +1,12 @@
 from django.contrib import admin
-from .models import LichLamViec, LichLamViec_CT
+from .models import LichLamViec
 
 # Register your models here.
 @admin.register(LichLamViec)
 class LichLamViecAdmin(admin.ModelAdmin):
-    list_display = ('ma_llv', 'ngay_lam', 'ca_lam', 'trang_thai', 'ngay_tao')
+    list_display = ('ma_llv', 'ngay_lam', 'ca_lam', 'ghi_chu', 'ma_chi_nhanh', 'trang_thai', 'ngay_tao')
     search_fields = ('ma_llv', 'ngay_lam')
     list_filter = ('ca_lam', 'trang_thai')
     ordering = ('ma_llv',)
 
-@admin.register(LichLamViec_CT)
-class LichLamViec_CTAdmin(admin.ModelAdmin):
-    list_display = ('ma_llv', 'ma_nv', 'vi_tri_vl')
-    search_fields = ('ma_llv', 'ma_nv')
-    list_filter = ('vi_tri_vl',)
-    ordering = ('ma_llv', 'ma_nv')
+
